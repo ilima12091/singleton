414853d866eda2628a89c3516d760264c657df36
ilima12091 <limaignacio12091@gmail.com>
2020-09-25 21:06:01 -0300

    Agrego los archivos

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,5 @@
-﻿using Library;
+﻿using System;
+using Library;
 
 namespace Program
 {
@@ -6,7 +7,11 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Greeter.Instance.SayHiToTheWorld();
+            Singleton<Greeter>.Instance.SayHiToTheWorld();
+            if(Singleton<Greeter>.Instance.Equals(Singleton<Greeter>.Instance))
+            {
+                Console.WriteLine("son iguales");
+            }
         }
     }
 }
